c515e480ce0919bfb667e61a7e866e4c6a277ce5
Claydor <clemens.daum@me.com>
2017-04-03 15:15:53 +0200

    Pixelwerte: Ausgabe hinzugefügt

--- pixelwerte.java
+++ pixelwerte.java
@@ -1,27 +1,43 @@
+import java.io.File;
 import java.awt.Color;
+import java.io.IOException;
+import javax.imageio.ImageIO;
 import java.awt.image.BufferedImage;
 
 public class pixelwerte {
-	
-	public static void main ( String[] args ) throws Exception {
-		
+
+	public static void main ( String[] args ) {
+
+		/*
 		int rgb;
 		Color c = new Color(rgb);
-		
-		BufferedImage img = ImageIO.read( new File("/home/nwboxuser/Dokumente/allerlei/bild.jpg") );
-       
-		int imageWidth = image.getWidth(pixelwerte);
-		int imageHeight =  image.getHeight(pixelwerte);
-		
-		int[][] farbwerte  = new int[imageWidth] [imageHeight];
-		
-		for ( int x = 0; x < imageWidth; x++ ) {
-		
-			for ( int y = 0; x < imageHeight; y++ ) {
-			
-					
+		*/
+
+		BufferedImage img = null;
+		try {
+			img = ImageIO.read(new File("Bild.jpg"));
+		} catch (IOException e) {}
+
+
+		int imageWidth = img.getWidth();
+		int imageHeight = img.getHeight();
+
+		int[][] farbwerte = new int[imageWidth] [imageHeight];
+
+		for (int x = 0; x < imageWidth; x++) {
+			for (int y = 0; y < imageHeight; y++) {
+				farbwerte[x][y] = img.getRGB(x,y);
 			}
 		}
-        rgb = img.getRGB(100, 50);
+
+		for (int x = 0; x < imageWidth; x++) {
+			for (int y = 0; y < imageHeight; y++) {
+				if (x == 0 && y == 0) {
+					System.out.println("\n");
+				}
+				System.out.print(x + ":" + y + " " + farbwerte[x][y] + "\t");
+			}
+			System.out.print("\n");
+		}
 	}
 }
